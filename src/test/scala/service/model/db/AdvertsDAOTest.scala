10800e372457021f2e703aa649f37149c8c3ae07
asoloviov@office <asoloviov@afkxxx.com>
2017-06-29 16:28:16 +0300

    AdvertsDAO simple test

--- src/test/scala/service/model/db/AdvertsDAOTest.scala
+++ src/test/scala/service/model/db/AdvertsDAOTest.scala
@@ -17,27 +17,42 @@ import scala.concurrent.duration._
 class AdvertsDAOTest extends WordSpec with ElasticSugar with Eventually with Matchers with SearchMatchers with TestData {
   implicit val duration: FiniteDuration = 10.seconds
   implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
+
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  val appConfig = AppConfig()
-  val dbContext = new DBContext(() => client,"test_", appConfig)
-  val advertsDAO = dbContext.advertsDAO
-  val indexName = advertsDAO.indexName
+  trait Context {
+    val appConfig = AppConfig()
+    val dbContext = new DBContext(() => client, "test_", appConfig)
+    val advertsDAO = dbContext.advertsDAO
+    val indexName = advertsDAO.indexName
+  }
+
   "AdvertsDAO" should {
-    "use configured ES client" in {
+    "use configured ES client" in new Context {
       advertsDAO.client should be theSameInstanceAs client
     }
-    "initialize index with mappings" in {
-      eventually(advertsDAO.checkOrCreateIndex()).await shouldEqual true
+    "initialize index with mappings" in new Context {
+      advertsDAO.checkOrCreateIndex().await shouldEqual true
     }
-    "store AD entity to ES " in {
+    "store AD entity to ES " in new Context {
+      val result = advertsDAO.create(oldCarAD).await
+      blockUntilCount(1, indexName)
+      result shouldEqual oldCarAD
+    }
+    "retrieve stored entity" in new Context {
       advertsDAO.create(oldCarAD).await shouldEqual oldCarAD
       blockUntilCount(1, indexName)
-      succeed
+      advertsDAO.getByID(oldCarAD.id.get).await shouldEqual Some(oldCarAD)
     }
-    "retrieve stored entity" in {
-      advertsDAO.create(oldCarAD).await shouldEqual oldCarAD
+    "delete stored entity" in new Context {
+      advertsDAO.create(newCarAD).await shouldEqual newCarAD
       blockUntilCount(1, indexName)
+      advertsDAO.deleteByID(newCarAD.id.get).await shouldEqual true
+    }
+    "update stored entity" in new Context {
+      advertsDAO.create(newCarAD).await shouldEqual newCarAD
+      blockUntilCount(1, indexName)
+      advertsDAO.updateByID(oldCarAD.id.get,oldCarAD.toUpdateAD).await shouldEqual Some(oldCarAD)
       advertsDAO.getByID(oldCarAD.id.get).await shouldEqual Some(oldCarAD)
     }
   }
